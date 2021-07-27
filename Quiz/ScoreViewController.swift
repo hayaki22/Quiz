828aa5c26efae84459bcf566a864d16e0b3e9776
hayaki22 <hayaki22vn@gmail.com>
2021-07-27 12:08:53 +0200

    QuizApp用のレポジトリ

---
+++ Quiz/ScoreViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ScoreViewController.swift
+//  Quiz
+//
+//  Created by Hayaki Maruta on 25.07.21.
+//
+
+import UIKit
+
+class ScoreViewController: UIViewController {
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var correct = 0
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        scoreLabel.text = "\(correct)問正解！"
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func toTopButtonAction(_ sender: Any) {
+        self.presentingViewController?.presentingViewController?.dismiss(animated: true)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
